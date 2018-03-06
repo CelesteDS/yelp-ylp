de17f82ed2c254f7f5e5cca0f8d9656a7e1505e0
Celeste De Santiago <celestedaphne@gmail.com>
2018-03-06 13:24:54 -0800

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
+import Restaurant from './restaurant'
+import RestaurantSelectButtons from './restaurant_select_buttons'
+import '../yelp_api'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Restaurant name="Burma Superstar"
+                    rating="5"
+                    location="Oakland"
+                    cost="$$"/>
+        <RestaurantSelectButtons />
+      </div>
     );
   }
 }
+
+/*
+Specs:
+- As a user I should be able to select a restaurant
+- once I select a restaurant I should see the details of the restaurant
+- as a user, I should not see a restaurant more than once
+
+
+App's state
+{
+  restaurantsSeenIds: ['abcd', 'defg', ...],
+  currentRestaurant: {},
+  restaurantToBeSeen: [],
+  restaurantSelected: {},
+}
+ */
